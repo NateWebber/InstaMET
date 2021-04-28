8c5d8e40b697910acadc5519350d7c3038a0d084
NateWebber <nwebber@u.rochester.edu>
2021-04-28 16:47:48 -0400

    initial commit, set up basic theme/colors

---
+++ app/src/main/java/com/nwebber/instamet/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nwebber.instamet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nwebber.instamet.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
