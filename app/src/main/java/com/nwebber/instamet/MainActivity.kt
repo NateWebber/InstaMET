ef40237087cacb97532a8d5715279b336d651b4e
NateWebber <nwebber@u.rochester.edu>
2021-04-28 17:51:59 -0400

    fixed navigation, added strings for both locales

--- app/src/main/java/com/nwebber/instamet/MainActivity.kt
+++ app/src/main/java/com/nwebber/instamet/MainActivity.kt
@@ -2,17 +2,29 @@ package com.nwebber.instamet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.nwebber.instamet.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController)
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            supportActionBar?.let {
+                it.title = when (destination.id) {
+                    R.id.settingsFragment -> getString(R.string.settings)
+                    R.id.aboutFragment -> getString(R.string.about)
+                    R.id.resultFragment -> getString(R.string.search_results)
+                    else -> getString(R.string.app_name)
+                }
+            }
         }
+
     }
 }
